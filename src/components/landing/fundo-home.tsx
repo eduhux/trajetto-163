4336dce2fc1e9f6a66e91f7d7450dc166a71f3a3
eduhux <eduhdesign.ux@gmail.com>
2026-06-17 16:55:13 -0400

    style: paleta de fundo fria (neutra) para valorizar o vidro

--- src/components/landing/fundo-home.tsx
+++ src/components/landing/fundo-home.tsx
@@ -6,9 +6,9 @@
 export function FundoHome() {
   return (
     <div aria-hidden className="pointer-events-none fixed inset-0 z-0 overflow-hidden">
-      <div className="absolute left-[6%] top-[10%] size-[40rem] rounded-full bg-trajetto/[0.07] blur-[150px]" />
-      <div className="absolute right-[2%] top-[40%] size-[34rem] rounded-full bg-cyan-400/[0.05] blur-[160px]" />
-      <div className="absolute bottom-[4%] left-1/2 size-[44rem] -translate-x-1/2 rounded-full bg-trajetto/[0.05] blur-[160px]" />
+      <div className="absolute left-[6%] top-[10%] size-[40rem] rounded-full bg-cyan-500/[0.06] blur-[150px]" />
+      <div className="absolute right-[2%] top-[38%] size-[36rem] rounded-full bg-sky-600/[0.06] blur-[160px]" />
+      <div className="absolute bottom-[4%] left-1/2 size-[44rem] -translate-x-1/2 rounded-full bg-indigo-500/[0.05] blur-[170px]" />
     </div>
   );
 }
